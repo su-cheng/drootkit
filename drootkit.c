384c3a85cf48140dcdedd7a9176f5cb1bc452533
Zhu Yongji <101548231+YeeJow@users.noreply.github.com>
2023-03-11 10:40:42 +0800

    Add files via upload

--- drootkit.c
+++ drootkit.c
@@ -4,120 +4,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/resource.h>
 #include <bpf/libbpf.h>
 #include "drootkit.skel.h"
-#include "String.h"
+#include "ht.h"
+// #include "syscall.h"
 
-/* 
- * the implement of type String 
- */
-bool StrAssign(String *S, char **chars) {
-	if(S->ch) {
-		S->length = 0;
-		free(S->ch);
-		S->ch = NULL;
-	}
-    
-	int i;
-	char *c;
-
-	for(i = 0, c = *chars; *c; i++, c++);
-	if (!i) {
-		S->ch = NULL;
-		S->length = 0;
-	}
-	else {
-		S->ch = (char*)malloc(i * sizeof(char));
-		for(int j = 0; j < i; j++) {
-			S->ch[j] = (*chars)[j];
-		}
-		S->length = i;
-	}
-	return OK;
-}
-
-unsigned int Length(String S) {
-	return S.length;
-}
-
-/*compare S1 and S2 and if S1 > S2 return 1 else return 0*/
-bool Compare(String S1, String S2) {
-	int i, j;
-	for (i = 0, j = 0; i < S1.length || i < S2.length; i++, j++) {
-		if (S1.ch[i] > S2.ch[j]) {
-			return 1;
-		}
-		else if (S1.ch[i] < S2.ch[j]) {
-			return 0;
-		}
-		else {
-			continue;
-		}
-	}
-	if(S1.length > S2.length) 
-		return 1;
-	else
-		return 0;
-}
-
-/*empty the string*/
-void ClearString(String *S) {
-	S->length = 0;
-	free(S->ch);
-	S->ch = NULL;
-}
-
-/*concat S1 and S2 and return the value using T*/
-bool Concat(String *S, String S1, String S2) {
-	S->ch = (char*)malloc((S1.length + S2.length) * sizeof(char));
-	S->length = S1.length + S2.length;
-	for (int i = 0; i < S1.length; i++) {
-		S->ch[i] = S1.ch[i];
-	}
-	for (int i = 0; i < S2.length; i++) {
-		S->ch[i + S1.length] = S2.ch[i];
-	}
-	return OK;	
-}
-
-/*return a substring of S whose length is len from the position of pos*/
-String SubString(String S, int pos, int len) {
-	String T;
-	T.length = len;
-	T.ch = (char*)malloc(len*sizeof(char));
-	for (int i = pos - 1; i < pos - 1 + len; i++) {
-		T.ch[i - pos + 1] = S.ch[i];
-	}
-	return T;
-}
-
-void Traverse(String S) {
-	if (S.ch == NULL)
-		printf("null");
-	else {
-		for (int i = 0; i < S.length; i++) {
-			printf("%c", S.ch[i]);
-		}
-	}
-	printf("\n");
-} 
+/* The maximum length of the file /proc/kallsyms line */
+#define LINE_MAX 256
 
 /* the kernel symbol map by parsing the /proc/kallsyms file.
  * each line contains the symbol's address, segment type, name, module owner (which can be empty in case the symbol is owned by the system)
  */
-typedef struct kernel_symbol{
-	char name[64];
-	char type[2];
+typedef struct kernel_symbol
+{
+	char *name;
+	char *type;
 	unsigned long long address;
-	char owner[64];
-}KernelSymbol;
+	char *owner;
+} KernelSymbol;
 
-// typedef struct kernel_symbol_table{
-// 	symbolMap     map[string]KernelSymbol
-// 	symbolAddrMap map[uint64]KernelSymbol
-// 	bool initialized;
-// }KernelSymbolTable;
+unsigned long long int str_2_addr(char *str, int len)
+{
+	unsigned long long int ans = 0;
+	for (int i = 0; i < len; ++i)
+	{
+		ans = ans * 10 + (str[i] - '0');
+	}
+	return ans;
+}
+
+/* the kernel_symbol_table holds two mapping tables:
+ * 1. symbol_map is used to find symbols by name
+ * 2. symbol_addr_map is used to find symbols by address
+ */
+typedef struct kernel_symbol_table
+{
+	ht *symbol_map;		 // char*, KernelSymbol
+	ht *symbol_addr_map; // uint64, KernelSymbol
+	bool initialized;
+} KernelSymbolTable;
 
 static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
 {
@@ -140,8 +67,10 @@ ssize_t get_uprobe_offset(const void *addr)
 	if (!f)
 		return -errno;
 
-	while (fscanf(f, "%zx-%zx %s %zx %*[^\n]\n", &start, &end, buf, &base) == 4) {
-		if (buf[2] == 'x' && (uintptr_t)addr >= start && (uintptr_t)addr < end) {
+	while (fscanf(f, "%zx-%zx %s %zx %*[^\n]\n", &start, &end, buf, &base) == 4)
+	{
+		if (buf[2] == 'x' && (uintptr_t)addr >= start && (uintptr_t)addr < end)
+		{
 			found = true;
 			break;
 		}
@@ -173,19 +102,138 @@ int main(int argc, char **argv)
 
 	/* Load and verify BPF application */
 	skel = drootkit_bpf__open_and_load();
-	if (!skel) {
+	if (!skel)
+	{
 		fprintf(stderr, "Failed to open and load BPF skeleton\n");
 		return 1;
 	}
 
-    /* ksymbols_map initial */
-    struct bpf_map *bpf_ksymbols_map;
-    bpf_ksymbols_map = bpf_object__find_map_by_name(skel->obj, "ksymbols_map");
-    if (!bpf_ksymbols_map) {
+	/* ksymbols_map initial */
+	struct bpf_map *bpf_ksymbols_map;
+	bpf_ksymbols_map = bpf_object__find_map_by_name(skel->obj, "ksymbols_map");
+	if (!bpf_ksymbols_map)
+	{
 		err = -errno;
 		fprintf(stderr, "Failed to find ksymbols_map: %d\n", err);
 		goto cleanup;
 	}
+	/* 限定需要放入hash表的符号范围 */
+
+	/* 创建两个hash表*/
+	KernelSymbolTable symbol_table;
+	symbol_table.symbol_map = ht_create();
+	// symbol_table.symbol_addr_map = ht_create();
+	symbol_table.initialized = false;
+
+	char buf[LINE_MAX] = {0};
+	char line[4][LINE_MAX] = {0};
+	int line_len = 0;
+	int line_num = 0;
+	FILE *f = fopen("/proc/kallsyms", "r");
+	if (!f)
+	{
+		fprintf(stderr, "could not open /proc/kallsyms\n");
+		goto cleanup;
+	}
+	int line_pnum = 0, insert_num = 0;
+	while (fgets(buf, LINE_MAX, f))
+	{
+		KernelSymbol *symbol = malloc(sizeof(KernelSymbol));
+		memset(line, 0, sizeof(line));
+		line_len = strlen(buf);
+		line_num = 0;
+		line_pnum++;
+
+		if ('\n' == buf[line_len - 1])
+		{
+			line_len--;
+			if (0 == line_len)
+			{
+				continue;
+			}
+		}
+
+		/* Splits a string, filling KernelSymbol */
+		bool is_word;
+		for (int i = 0; i <= line_len; ++i)
+		{
+			if (buf[i] != ' ' && buf[i] != '\t')
+			{
+				is_word = 1;
+				line_num++;
+			}
+			int j = 0;
+			while (is_word)
+			{
+				line[line_num - 1][j++] = buf[i++];
+				if (buf[i] == ' ' || buf[i] == '\t' || buf[i] == '\n')
+				{
+					is_word = 0;
+					line[line_num - 1][j] = '\0';
+				}
+			}
+			if (buf[i] == '\n')
+			{
+				break;
+			}
+		}
+		if (line_num < 3)
+		{
+			continue;
+		}
+		symbol->address = str_2_addr(line[0], strlen(line[0]));
+		symbol->type = malloc(strlen(line[1]) + 1);
+		strcpy(symbol->type, line[1]);
+		symbol->name = malloc(strlen(line[2]) + 1);
+		strcpy(symbol->name, line[2]);
+		if (line_num == 3)
+		{
+			symbol->owner = malloc(strlen("[system]") + 1);
+			strcpy(symbol->owner, "[system]");
+		}
+		else
+		{
+			symbol->owner = malloc(strlen(line[3]) + 1);
+			strcpy(symbol->owner, line[3]);
+		}
+
+		FILE *tf = fopen("./1.txt", "a");
+		fprintf(tf, "%llu %s %s %s\n", symbol->address, symbol->type, symbol->name, symbol->owner);
+		fclose(tf);
+
+		/*Insert KernelSymbol into symbol_map*/
+		if (ht_set(symbol_table.symbol_map, symbol->name, (void *)symbol) == NULL)
+		{
+			fprintf(stderr, "out of memory\n");
+			goto cleanup;
+		}
+		else
+		{
+			/* 			void *value = ht_get(symbol_table.symbol_map, symbol->name);
+						printf("%llu %s %s %s\n", ((KernelSymbol *)value)->address, ((KernelSymbol *)value)->type, ((KernelSymbol *)value)->name, ((KernelSymbol *)value)->owner); */
+			insert_num++;
+		}
+		/*Insert KernelSymbol into symbol_addr_map*/
+	}
+	FILE *ttf = fopen("./2.txt", "a");
+
+	hti it = ht_iterator(symbol_table.symbol_map);
+	while (ht_next(&it))
+	{
+		// printf("%llu %s %s %s\n", ((KernelSymbol *)it.value)->address, ((KernelSymbol *)it.value)->type, ((KernelSymbol *)it.value)->name, ((KernelSymbol *)it.value)->owner);
+		//  fprintf(ttf, "%d %s:", it._index, it.key);
+		fprintf(ttf, "%llu %s %s %s\n", ((KernelSymbol *)it.value)->address, ((KernelSymbol *)it.value)->type, ((KernelSymbol *)it.value)->name, ((KernelSymbol *)it.value)->owner);
+	}
+	fclose(ttf);
+	printf("insert_num=%d line_allnum=%d ht_lenght=%lu\n", insert_num, line_pnum, ht_length(symbol_table.symbol_map));
+
+	if (0 == feof(f))
+	{
+		fprintf(stderr, "fgets error: The end of the file was not read\n");
+		goto cleanup;
+	}
+	symbol_table.initialized = true;
+	fclose(f);
 
 	/* uprobe/uretprobe expects relative offset of the function to attach
 	 * to. This offset is relateve to the process's base load address. So
@@ -198,11 +246,12 @@ int main(int argc, char **argv)
 
 	/* Attach tracepoint handler */
 	skel->links.Syscalls_Intergrity_Check_Entry = bpf_program__attach_uprobe(skel->progs.Syscalls_Intergrity_Check_Entry,
-							    false /* not uretprobe */,
-							    0 /* self pid */,
-							    "/proc/self/exe",
-							    uprobe_offset);
-	if (!skel->links.Syscalls_Intergrity_Check_Entry) {
+																			 false /* not uretprobe */,
+																			 0 /* self pid */,
+																			 "/proc/self/exe",
+																			 uprobe_offset);
+	if (!skel->links.Syscalls_Intergrity_Check_Entry)
+	{
 		err = -errno;
 		fprintf(stderr, "Failed to attach uprobe: %d\n", err);
 		goto cleanup;
@@ -213,24 +262,26 @@ int main(int argc, char **argv)
 	 * to specify -1 as PID, as we do here
 	 */
 	skel->links.Syscalls_Intergrity_Check_Ret = bpf_program__attach_uprobe(skel->progs.Syscalls_Intergrity_Check_Ret,
-							       true /* uretprobe */,
-							       -1 /* any pid */,
-							       "/proc/self/exe",
-							       uprobe_offset);
-	if (!skel->links.Syscalls_Intergrity_Check_Ret) {
+																		   true /* uretprobe */,
+																		   -1 /* any pid */,
+																		   "/proc/self/exe",
+																		   uprobe_offset);
+	if (!skel->links.Syscalls_Intergrity_Check_Ret)
+	{
 		err = -errno;
 		fprintf(stderr, "Failed to attach uprobe: %d\n", err);
 		goto cleanup;
 	}
 
 	printf("Successfully started! Please run `sudo cat /sys/kernel/debug/tracing/trace_pipe` "
-	       "to see output of the BPF programs.\n");
+		   "to see output of the BPF programs.\n");
 
-    for (;;) {
-            /* trigger our BPF program */
-            SyscallsIntergrityCheck();
-            sleep(1);
-        }
+	for (;;)
+	{
+		/* trigger our BPF program */
+		SyscallsIntergrityCheck();
+		sleep(1);
+	}
 
 cleanup:
 	drootkit_bpf__destroy(skel);
